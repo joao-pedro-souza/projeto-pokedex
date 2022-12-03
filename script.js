e020c4a07ff8e18c1aace18f9be9bef907cecabe
João Pedro Souza <56794682+joao-pedro-souza@users.noreply.github.com>
2022-12-03 10:07:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const pokemonName = document.querySelector('.pokemon_name');
+const pokemonNumber = document.querySelector('.pokemon_number');
+const pokemonImage = document.querySelector('.pokemon_image');
 
 const fetchPokemon = async (pokemon) => {
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
@@ -10,7 +12,7 @@ const renderPokemon = async (pokemon) => {
 
     const data = await fetchPokemon(pokemon);
     pokemonName.innerHTML = data.name;
-
+    pokemonNumber.innerHTML = data.id;
 }
 
-renderPokemon('25')
+renderPokemon('6')

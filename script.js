72886237f9b2014eab575074540e2f89ca8e5f82
João Pedro Souza <56794682+joao-pedro-souza@users.noreply.github.com>
2022-12-03 11:23:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const pokemonNumber = document.querySelector('.pokemon_number');
 const pokemonImage = document.querySelector('.pokemon_image');
 const form = document.querySelector('.form');
 const input = document.querySelector('.input_search');
+const next = document.querySelector('.btn-next');
+const prev = document.querySelector('.btn-prev');
+let searchPokemon = 1;
 
 const fetchPokemon = async (pokemon) => {
     pokemonName.innerHTML = 'Loading...'
@@ -27,6 +30,7 @@ const renderPokemon = async (pokemon) => {
     pokemonName.innerHTML = data.name;
     pokemonNumber.innerHTML = data.id;
     pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+    searchPokemon = data.id
 }
 
 form.addEventListener('submit', (event) => {
@@ -35,4 +39,17 @@ form.addEventListener('submit', (event) => {
     input.value = '';
 });
 
-renderPokemon('1')
+
+next.addEventListener('click', () => {
+        searchPokemon += 1;
+        renderPokemon(searchPokemon);
+});
+
+prev.addEventListener('click', () => {
+    if (searchPokemon>1){
+    searchPokemon -= 1;
+    renderPokemon(searchPokemon);
+    }
+});
+
+renderPokemon(searchPokemon)
